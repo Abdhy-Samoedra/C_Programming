d3ca4a63b206cbf09bb1e1faaf3066f71bbdf134
abdhy <abdhysamoedra030@gmail.com>
2022-12-15 09:32:16 +0700

    update

--- data structure/Session2.cpp
+++ data structure/Session2.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// head akan selalu terdapat pada data yang terakhir kita insert
+
 struct node
 {
     int val;
     struct node *next;
 } *head = NULL;
 
-void insertNode(int val)
+struct node *newNode(int val)
 {
     // mallocnya harus dicasting dulu
     struct node *temp = (struct node *)malloc(sizeof(struct node));
@@ -15,6 +17,12 @@ void insertNode(int val)
     temp->val = val;
     temp->next = NULL;
 
+    return temp;
+}
+
+void insertNode(int val)
+{
+    struct node *temp = newNode(val);
     // seiap kali insert harus melakukan validasi. apakah kita memasukkan data yang pertama atau data yang kedua lebih
     // insert data pertama
     if (head == NULL)
@@ -25,9 +33,47 @@ void insertNode(int val)
     else
     {
         temp->next = head;
-        head = temp;            
+        head = temp;
     }
-    
+}
+
+void dsiplayAll()
+{
+    // membuat pointer untuk menunjuk data pertama
+    struct node *curr = head;
+
+    while (curr != NULL)
+    {
+        printf("%d\n", curr->val);
+
+        // menggeser currentnya agar tidak berhenti di head
+        curr = curr->next;
+    }
+}
+
+void deleteNode(int val)
+{
+    struct node *curr = head;
+
+    // jika yang mau didelet ada di head
+    if (head->val == val)
+    {
+        head = curr->next;
+        free(curr);
+    }
+
+    // jika yang mau dihapus ada di node kedua atau lebih
+    else
+    {
+        while (curr->next->val != val)
+        {
+            curr = curr->next;
+        }
+
+        struct node *del = curr->next;
+        curr->next = del->next;
+        free(del);
+    }
 }
 
 int main()
@@ -36,4 +82,8 @@ int main()
     insertNode(2);
     insertNode(5);
     insertNode(1);
+
+    deleteNode(2);
+
+    dsiplayAll();
 }
